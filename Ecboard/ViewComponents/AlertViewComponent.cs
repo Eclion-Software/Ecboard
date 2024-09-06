ad1e0d5405393ca80e15fe0944cab8914e9299e0
Kerem Aydın <suspectcrow@outlook.com.tr>
2024-09-06 14:44:03 +0300

    Alert component created

--- Ecboard/ViewComponents/AlertViewComponent.cs
+++ Ecboard/ViewComponents/AlertViewComponent.cs
@@ -2,15 +2,40 @@
 using Ecboard.Services;
 using Ecboard.ViewModel.Interface;
 using Ecboard.ViewModels;
+using Ecboard.Enum;
 
 namespace Ecboard.ViewComponents
 {
     public class AlertViewComponent : ViewComponent
     {
-        public async Task<IViewComponentResult> InvokeAsync(AlertViewModel model)
+        //public async Task<IViewComponentResult> InvokeAsync(AlertViewModel model)
+        //{
+
+        //    return View("_pAlert", model);
+        //}
+
+        public async Task<IViewComponentResult> InvokeAsync(string name, Enum_Color color)
         {
 
-            return View("_pAlert", model);
+            return View("_pAlert", new AlertViewModel()
+            {
+                AlertType = Enum.Enum_AlertType.Default,
+                Color = color,
+                Description = "",
+                Name = name
+            });
         }
+
+        //public async Task<IViewComponentResult> InvokeAsync(string name)
+        //{
+
+        //    return View("_pAlert", new AlertViewModel()
+        //    {
+        //        AlertType = Enum.Enum_AlertType.Default,
+        //        Color = Enum.Enum_Color.secondary,
+        //        Description = "",
+        //        Name = name
+        //    });
+        //}
     }
 }
